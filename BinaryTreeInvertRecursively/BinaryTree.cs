65f9f2fe7ddfd03ea3b7adc67ef37a30de49ef9d
zmil44 <zachary.j.miller@outlook.com>
2018-09-10 13:25:35 -0400

    Finished inverting a binary tree recursively

---
+++ BinaryTreeInvertRecursively/BinaryTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeInvertRecursively
+{
+    class BinaryTree
+    {
+        public int value;
+        public BinaryTree left;
+        public BinaryTree right;
+
+        public BinaryTree(int[] values) : this(values, 0) { }
+
+        BinaryTree(int[] values, int index)
+        {
+            Load(this, values, index);
+        }
+
+        void Load(BinaryTree tree, int[] values, int index)
+        {
+            this.value = values[index];
+            if (index * 2 + 1 < values.Length)
+            {
+                
+                this.left = new BinaryTree(values, index * 2 + 1);
+            }
+            if (index * 2 + 2 < values.Length)
+            {
+                this.right = new BinaryTree(values, index * 2 + 2);
+            }
+        }
+    }
+}
--- BinaryTreeInvertRecursively/Program.cs
+++ BinaryTreeInvertRecursively/Program.cs
@@ -10,6 +10,36 @@ namespace BinaryTreeInvertRecursively
     {
         static void Main(string[] args)
         {
+            int[] values = new int[] { 7,6,5,4,3,2,1};
+            BinaryTree tree = new BinaryTree(values);
+            Console.WriteLine("Before Invert: ");
+            Console.WriteLine($"\t\t\t {tree.value}");
+            Console.WriteLine("\t\t\t/\\");
+            Console.WriteLine("\t\t       /  \\");
+            Console.WriteLine($"\t\t      {tree.left.value}    {tree.right.value}");
+            Console.WriteLine("\t\t     /\\\t  /\\");
+            Console.WriteLine($"\t\t    {tree.left.left.value} {tree.left.right.value}\t {tree.right.left.value} {tree.right.right.value}");
+            InvertTree(tree);
+            Console.WriteLine("After Invert: ");
+            Console.WriteLine($"\t\t\t {tree.value}");
+            Console.WriteLine("\t\t\t/\\");
+            Console.WriteLine("\t\t       /  \\");
+            Console.WriteLine($"\t\t      {tree.left.value}    {tree.right.value}");
+            Console.WriteLine("\t\t     /\\\t  /\\");
+            Console.WriteLine($"\t\t    {tree.left.left.value} {tree.left.right.value}\t {tree.right.left.value} {tree.right.right.value}");
+
+        }
+
+        static BinaryTree InvertTree(BinaryTree tree)
+        {
+            if (tree == null) return null;
+
+            var left = tree.left;
+            tree.left = InvertTree(tree.right);
+            tree.right = InvertTree(left);
+            return tree;
+        }
+
         }
     }
 }
